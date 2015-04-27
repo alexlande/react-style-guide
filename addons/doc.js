51ea8119b47632cfb11cf24598e18767be87628c
Rob Gerstenberger <rgerstenberger@walmartlabs.com>
2015-04-27 16:28:57 -0700

    fix isRequire typo

--- addons/doc.js
+++ addons/doc.js
@@ -37,7 +37,7 @@ var propTypesArray = [{
 var getReactPropType = function (propTypeFunc) {
   var propType = {
     name: 'custom',
-    isRequire: false
+    isRequired: false
   };
 
   for (var i = 0; i < propTypesArray.length; i++) {
